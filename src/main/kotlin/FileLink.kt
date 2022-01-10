eb19079f085571089b91b63aff308ca0440f1da4
Bohdan00735 <bodya00735@gmail.com>
2022-01-10 13:28:13 +0200

    init

--- src/main/kotlin/FileLink.kt
+++
@@ -1,7 +0,0 @@
-class FileLink(var fileName: String, val descriptorLink: Int) {
-    private var isLinkValid = true
-
-    override fun toString(): String {
-        return "$fileName:$descriptorLink:$isLinkValid"
-    }
-}
